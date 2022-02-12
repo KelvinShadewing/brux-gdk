ee99ffc4275de2ded08a1eec206dce4e87eaa634
Kelvin Shadewing <kelvin@shadewing.net>
2022-02-12 18:41:42 -0500

    Add pi... seriously

--- rte/core.cpp
+++ rte/core.cpp
@@ -390,6 +390,7 @@ void xyLoadCore() {
 	const c_magenta = 0xff00ffff; \n\
 	const c_cyan = 0xffffff; \n\
 	const c_black = 0xff; \n\
+	const pi = 3.14159265358979323846; \n\
 	 \n\
 	arraySort <- function(arr){\n\t//Skip sorting if it's not an array\n\tif(typeof arr != \"array\") return arr;\n\n\t//or if there's nothing to sort\n\tif(arr.len() <= 1) return arr;\n\n\tlocal needsort = true;\n\twhile(needsort){\n\t\tneedsort = false;\n\t\tfor(local i = 0; i < arr.len() - 2; i++){\n\t\t\tif(arr[i] > arr[i+1]){\n\t\t\t\tlocal temp = arr[i];\n\t\t\t\tarr[i] = arr[i+1];\n\t\t\t\tarr[i+1] = temp;\n\t\t\t\tneedsort = true;\n\t\t\t};\n\t\t};\n\t};\n\n\treturn arr;\n}; \n\
 	 \n\
