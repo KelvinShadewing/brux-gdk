ee0ce04acc831ab0b39647a4119ce1ebf371ddf2
tulip-sudo <noctaliavibing@gmail.com>
2022-12-17 22:36:40 +0000

    Made the file system view display the current directory

--- ide/editorwindow.cpp
+++ ide/editorwindow.cpp
@@ -1,8 +1,19 @@
 #include "editorwindow.h"
 #include "ui_editorwindow.h"
 
+#include <iostream>
+#include <QDir>
+
 EditorWindow::EditorWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::EditorWindow) {
 	ui->setupUi(this);
+
+	std::cout << Directory.toStdString() << std::endl;
+	DirectoryView.setFilter(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
+	DirectoryView.setRootPath(Directory);
+
+	auto TreeView = ui->centralwidget->findChild<QTreeView*>("treeView");
+	TreeView->setModel(&DirectoryView);
+	TreeView->setRootIndex(DirectoryView.index(Directory));
 
 	TextEditorInstance = KTextEditor::Editor::instance();
 	Document = TextEditorInstance->createDocument(this);
@@ -18,4 +29,14 @@ EditorWindow::EditorWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::Ed
 EditorWindow::~EditorWindow() {
 	delete ui;
 }
+
+bool EditorWindow::isFile(QString path) {
+	return false;
+}
+
+bool EditorWindow::isTilemap(QString path) {
+	return false;
+}
+
+void EditorWindow::openFile(QString Path) {}
 
--- ide/editorwindow.h
+++ ide/editorwindow.h
@@ -2,9 +2,17 @@
 #define EDITORWINDOW_H
 
 #include <QMainWindow>
+#include <QFileSystemModel>
 #include <KTextEditor/Document>
 #include <KTextEditor/Editor>
 #include <KTextEditor/View>
+
+#include <tiled/map.h>
+#include <tiled/maprenderer.h>
+#include <tiled/tiled.h>
+
+#include <vector>
+
 #include "ui_editorwindow.h"
 
 class EditorWindow : public QMainWindow
@@ -15,10 +23,19 @@ public:
 	EditorWindow(QWidget *parent = nullptr);
 	~EditorWindow();
 
+	bool isFile(QString path);
+
+	bool isTilemap(QString path);
+
+	void openFile(QString path);
 private:
 	Ui::EditorWindow *ui;
 	KTextEditor::Editor* TextEditorInstance;
 	KTextEditor::Document* Document;
 	KTextEditor::View* DocumentView;
+	QStringList DocumentTitles;
+	std::vector<Tiled::MapRenderer> Renderers;
+	QString Directory = getenv("PWD");
+	QFileSystemModel DirectoryView;
 };
 #endif // EDITORWINDOW_H
