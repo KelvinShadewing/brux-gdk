c5dbb664e95f55c663524b5a4a3dfaf5669814b4
Tulpenkiste <tulpenkiste@amogus.cloud>
2025-08-16 20:56:33 +0200

    Remove unused meson var, _build.sh now checks if a certain file exists and sets up the build dir if not

--- rte/_build.sh
+++ rte/_build.sh
@@ -10,6 +10,9 @@ fi
 
 if [[ ! -d build ]]; then
 	meson setup build
+elif [[ ! -f build/meson-info/meson-info.json ]]; then
+	echo Missing meson-info.json, resetting up...
+	meson setup build --wipe
 fi
 
 meson compile -C build
